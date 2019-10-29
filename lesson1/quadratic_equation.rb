8017fe232c455a34d8c502748fcfdec5ec31dd06
bulgakke <vvp835@yandex.ru>
2019-10-29 21:56:19 +0300

    последнее исправление c sqrt(D)

--- lesson1/quadratic_equation.rb
+++ lesson1/quadratic_equation.rb
@@ -6,16 +6,18 @@ b = gets.to_f
 puts "Enter the 'c' coefficient:"
 c = gets.to_f
 
-sqrtd = Math.sqrt(b * b - 4 * a * c)
 if d > 0 
+  sqrtd = Math.sqrt(b * b - 4 * a * c)
   x1 = (- b + sqrtd) / (2 * a)
   x2 = (- b - sqrtd) / (2 * a)
   puts "This equation's roots are #{x1} and #{x2}."
-elsif d < 0
-  puts 'This equation has no real roots.'
-else
+elsif d == 0
+  sqrtd = Math.sqrt(b * b - 4 * a * c) # можно эту строчку вычислить один раз, но тогда ветвление будет сложнее
   x = - b / (2 * a)
   puts "The only root this equation has is #{x}."
+  
+else
+  puts 'This equation has no real roots.'
 end
 
 # Расставлены пробелы вокруг операторов, корень из D вычисляется один раз, починены кавычки (только в строке 15) и табуляция, в конце - пустая строка.
