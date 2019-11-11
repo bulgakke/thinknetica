98dd15d58d8900a23b3f86f4d6710312ba9e0718
bulgakke <vvp835@yandex.ru>
2019-11-12 00:29:09 +0300

    Cleaned up, still doesn't work

--- lesson3/classes.rb
+++ lesson3/classes.rb
@@ -1,63 +1,55 @@
 class Station
+  attr_reader :trains, :freight_trains, :passenger_trains
   
   def initialize(name)
     @name = name
-    @trains = {}
+    @trains = []
+    @freight_trains = []
+    @passenger_trains = []
   end
 
   def get_train(train)
-    @trains[train] = train.type # вот это почему-то не работает. Если что, я пытаюсь дать ему объект класса "поезд" и сопоставить значением тип этого поезда
+    @trains << train
+    @freight_trains << train if train.type == :freight
+    @passenger_trains << train if train.type == :passenger 
+    # списки поездов разных типов можно было кучей разных способов реализовать, тут вроде нормально
   end
 
   def send_train(train)
     @trains.delete(train)
+    @freight_trains.delete(train)
+    @passenger_trains.delete(train)
   end
-  
-  def trains_here
-    return @trains.keys
-  end
-
-  def trains_of_type(train_type)
-    @amount_of_this_type = @trains.select { |train, type| type = train_type}
-    return @amount_of_this_type.size
-  end
-
 end
 
-
 class Route
-
-  def initialize(start_station, last_station)
-    @stations = [start_station, last_station]
+  attr_reader :stations
+  
+  def initialize(first_station, last_station)
+    @first_station = first_station
+    @last_station = last_station
+    @stations = [@first_station, @last_station]
   end
 
-  def add_station(name, index=-2)
-    @stations.insert(index, name) # по дефолту добавляет станцию предпоследней
+  def add_station(name, index=-2) 
+    # по дефолту добавляет станцию предпоследней
+    @stations.insert(index, name) if index != -1 && index != 0 
+    # не дает добавить её начальной/конечной
   end
 
   def delete_station(name)
-    @stations.delete(name)
+    @stations.delete(name) if name != @start_station && name != @last_station 
   end
-
-  def stations
-    return @stations # оставил в таком виде для целесообразности коммента
-    # я честно путаюсь между return, puts и просто указанием переменной внутри метода, помогите
-    # и нужно оно вообще, или достаточно attr_reader :stations ?
-  end
-
 end
-
 
 class Train
   attr_accessor :speed
-  attr_reader :number
-  attr_reader :type
-  attr_reader :wagons_amount
+  attr_reader :number, :type, :wagons, :station
   
-  def initialize(number, type, wagons_amount) # type = :freight || :passenger; можно ли и надо ли ограничивать ввод двумя этим опциями?
+  def initialize(number, type, wagons) 
     @number = number
     @type = type
-    @wagons_amount = wagons_amount
+    @wagons = wagons
     @speed = 0
   end
 
@@ -66,39 +58,57 @@ class Train
   end
 
   def add_wagon
-    @wagons_amount += 1 if @speed == 0 
+    @wagons += 1 if @speed == 0 
   end
-  
+
   def remove_wagon
-    @wagons_amount -= 1 if @speed == 0 && @wagons_amount > 1 # надо возвращать что-то, если поезд в движении или состоит из одного вагона?
+    @wagons -= 1 if @speed == 0 && @wagons > 1
   end
 
   def acquire_route(route)
-    @current_route = route
-    @prev_station = route.stations[-1] # допускаем, что маршрут кольцевой?
-    @current_station = route.stations[0]
-    @next_station = route.stations[1]
+    @route = route
+    @station = route.stations[0]
+    @station.get_train(self) # (!) здесь всё работает, в методах движения - нет
+    @index = @route.stations.index(@station) 
+    # смотрит индекс текущей станции в массиве маршрута, чтобы далее от него считать prev/next
   end
 
-  def status
-    puts "Current station is #{@current_station}"
-    puts "Next station is #{@next_station}"
-    puts "Previous station is #{@prev_station}"
+  def next_station
+    @route.stations[@index+1] if @station != @route.stations.last 
+    # так как маршрут не кольцевой
   end
+    
 
+  def prev_station
+    @route.stations[@index-1] if @station != @route.stations.first
+  end
+  
   def move_forward
-    @prev_station = @current_station
-    @current_station.send_train(self)
-    @current_station = @next_station
-    @next_station = @current_route.stations[@current_route.stations.index(@next_station)+1] # вот тут понятия не имею, как сделать лучше
-    @current_station.get_train(self)
+    @station.send_train(self)
+    @station = @next_station
+    @station.get_train(self)
+  end
+=begin
+Понятия не имею, почему, но оно не работает:
+classes.rb:88:in `move_forward': undefined method `get_train' for nil:NilClass (NoMethodError)
+см. строки 71 и 108
+Думал вместо def get_train написать def self.get_train (и аналогично с send_train) - не помогло (да и не должно было)
+
+Выборочно просмотрел гитхабы других людей с курса - 
+ну там либо точно такая же логика, либо вообще нет связи со станцией (методы send_train и get_train есть, но они не вызываются в методах move_forward/move_back)
+
+Можно просто убрать связь со станцией и потом каждый раз вручную в коде писать три строчки вместо одной, но как-то гм
+=end
+  def move_back # аналогично move_forward
+    @station.send_train(self)
+    @station = @prev_station
+    @station.get_train(self)
   end
 
-  def move_back
-    @next_station = @current_station
-    @current_station.send_train(self)
-    @current_station = @prev_station
-    @prev_station = @current_route.stations[@current_route.stations.index(@prev_station)-1]
-    @current_station.get_train(self)
+  def move_to(station) # сделал этот метод только для тестов, но он тоже работает
+    @station.send_train(self)
+    @station = station
+    @station.get_train(self)
   end
 end
+
