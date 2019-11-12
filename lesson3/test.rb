0e57773444f378a9a02142147b2cd26749a2882f
bulgakke <vvp835@yandex.ru>
2019-11-12 13:56:18 +0300

    до кучи

--- lesson3/test.rb
+++ lesson3/test.rb
@@ -5,13 +5,18 @@ stat2 = Station.new('stat2')
 stat3 = Station.new('stat3')
 абдырвалг = Station.new('абдырвалг')
 
-route1 = Route.new(stat1, stat2)
+route1 = Route.new(stat1, stat3)
+route1.add_station(stat2)
+route1.add_station(абдырвалг)
 
-route1.add_station('абдырвалг')
-route1.add_station('stat3')
-
-thomas = Train.new(228, :freight, 5)
-thomas.acquire_route(route1)
-thomas.move_back
-thomas.status
+train1 = Train.new(123, :freight, 5)
+train1.acquire_route(route1)
+puts train1.next_station
+puts '#'
+puts stat1.trains
+puts '#'
+train1.move_forward
+puts stat1.trains
+puts '#'
+puts stat2.trains
 
