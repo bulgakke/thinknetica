bc6c7d8b089b0827f5a86e5bb36afd2399ebe18d
bulgakke <vvp835@yandex.ru>
2019-12-10 19:54:07 +0300

    separate validation methods

--- lesson9/modules/validation.rb
+++ lesson9/modules/validation.rb
@@ -7,39 +7,22 @@ module Validation
   end
 
   module ClassMethods
+    
     @@validators ||= []
+
+    def validators 
+      @@validators
+    end
+
+    def validators_add(validator)
+      @@validators << validator 
+    end
+
     def validate(var, type, parameter = nil)
       validator = { variable_to_validate: var, type_of_validation: type, validation_parameter: parameter }
-      @@validators << validator
-
-      define_method('validate!') do
-        @@validators.each do |validator|
-          var_name = validator[:variable_to_validate]
-          var = instance_variable_get("@#{var_name}")
-          type = validator[:type_of_validation]
-          parameter = validator[:validation_parameter]
-          case type
-          when :presence
-            raise "#{var} can't be empty" if var.nil? || var.to_s.strip == ''
-          when :format
-            raise 'Use Regexp as a parameter' unless parameter.class == Regexp
-            # структурно строчка выше вроде не здесь должна быть, но понятия не имею, куда её деть
-            # тут должны быть исключения для юзера (как ниже), а куда класть исключения для кодера?
-            raise "Wrong number format for #{var}, should fit #{parameter}" if var !~ parameter
-          when :class
-            unless (parameter.class == Class) || (parameter[0] == Class)
-              raise 'Use a proper Class (or an array of them) as a parameter'
-            end
-            raise "Wrong #{var} class, should be #{parameter}" unless var.class == parameter || parameter.include?(var)
-          when :equals
-            raise "#{var} should be equal to #{parameter}" unless var == parameter || parameter.include?(var)
-          when :characters
-            raise "You can only use Latin and Cyrillic, numbers, spaces, '-' and '_'." if invalid_chars?(var) == true
-          else
-            raise "Wrong 'type' argument for 'validate', should be :presence/:format/:class"
-          end
-        end
-      end
+      # каким образом делать это переменной экземпляра, если validate - это метод класса? И параметры валидации общие для всех объектов класса?
+      # В какую переменную экземпляра он будет складывать параметры? В initialize что-то писать?
+      validators_add(validator)     
     end
   end
 
@@ -51,12 +34,60 @@ module Validation
       false
     end
 
-    protected
+    def validate!
+      self.class.validators.each do |validator|
+        var_name = validator[:variable_to_validate]
+        var = instance_variable_get("@#{var_name}")
+        type = validator[:type_of_validation]
+        parameter = validator[:validation_parameter]
+        case type
+        when :presence
+          validate_presence(var)
+        when :format
+          validate_format(var, parameter)
+        when :class
+          validate_class(var, parameter)
+        when :equals
+          validate_equals(var, parameter)
+        when :characters
+          validate_characters(var, parameter)
+        else
+          raise "Wrong 'type' argument for 'validate', should be :presence/:format/:class/:equals/:characters"
+        end
+      end
+    end
 
+    def validate_presence(var)
+      raise "#{var} can't be empty" if var.nil? || var.to_s.strip == ''
+    end
+
+    def validate_format(var, parameter)      
+      raise 'Use Regexp as a parameter' unless parameter.class == Regexp
+      raise "Wrong number format for #{var}, should fit #{parameter}" if var !~ parameter
+    end
+
+    def validate_class(var, parameter)
+      unless (parameter.class == Class) || (parameter[0] == Class)
+        raise 'Use a proper Class (or an array of them) as a parameter'
+      end
+
+      unless var.class == parameter || (parameter.class == Array && parameter.include?(var.class))
+        raise "Wrong #{var} class, should be #{parameter}"
+      end
+    end
+
+    def validate_equals(var, parameter)
+      raise "#{var} should be equal to #{parameter}" unless var == parameter || parameter.include?(var)
+    end
+
+    def validate_characters(var, parameter)
+      raise "You can only use Latin and Cyrillic, numbers, spaces, '-' and '_'." if invalid_chars?(var)
+    end
+
+    protected
+    
     def invalid_chars?(string)
       allowed = ('A'..'Z').to_a + ('a'..'z').to_a + ('А'..'Я').to_a + ('а'..'я').to_a + ('0'..'9').to_a + ['-', '_', ' ']
-      # ничего, что я в таких случаях return пишу?
-      # и самому понятнее, что происходит, и вообще на всякий случай
       string.chars.each do |character|
         return true if allowed.include?(character) == false
       end
@@ -64,16 +95,17 @@ module Validation
     end
   end
 end
-# class TestClass
-#   include Validation
-#   attr_accessor :name, :weight, :length
-#   validate :name, :format, /^[A-ZА-Я]{1}[a-zа-я]+$/
-#   validate :weight, :class, Integer
-#   validate :length, :presence
-# end
-#
-# t = TestClass.new
-# t.name = 'Qwe'
-# t.weight = 123
-# t.length = ''
-# puts t.valid? # => true/false
+
+class TestClass
+  include Validation
+  attr_accessor :name, :weight, :length
+  validate :name, :format, /^[A-ZА-Я]{1}[a-zа-я]+$/
+  validate :weight, :class, Integer
+  validate :length, :presence
+end
+
+t = TestClass.new
+t.name = 'Qwe'
+t.weight = 123
+t.length = ' '
+t.validate!
--- lesson9/test.rb
+++ lesson9/test.rb
@@ -1,44 +1,16 @@
-# frozen_string_literal: true
+module ClassMethods
+  attr_accessor :validations
 
-module Validation
-  def validate(*types)
-    types.each do |type|
-      define_method("validate_#{type}") do |var, parameter = nil|
-        var = instance_variable_get("@#{var}")
-
-        if type == :presense
-          raise "#{var} can't be empty" if var.nil? || var.to_s.strip == ''
-        elsif type == :format
-          raise 'Use Regexp as a parameter' unless parameter.class == Regexp
-          # структурно строчка выше вроде не здесь должна быть, но понятия не имею, куда её деть
-          # тут должны быть исключения для юзера (как ниже), а куда класть исключения для кодера?
-          raise "Wrong number format for #{var}, should fit #{parameter}" if var !~ parameter
-        elsif type == :class
-          raise 'Use proper Class name as a parameter' unless parameter.class == Class
-          raise "Wrong #{var} class, should be #{parameter}" unless var.class == parameter
-        else
-          raise "Wrong arguments for 'validate', should be [:presense/:format/:type]"
-        end
-        print 'SUCCESS ' # debug
-      end
-    end
+  def validate(name, type, arg = "")
+    self.validations ||= []
+    rule = { type => { name: name, arg: arg } }
+    validations << rule
   end
 end
 
-class Test
-  extend Validation
-  attr_accessor :number
-  validate :class, :presense, :format
-  FORMAT = /^[a-zа-я\d]{3}(-[a-zа-я\d]{2})?$/.freeze
-  /^[A-ZА-Я]{1}[a-zа-я]{1+}$/
-
-  def validate_all(var)
-    validate_class(var, String)
-    validate_presense(var)
-    validate_format(var, FORMAT)
-  end
+class Klass
+  extend ClassMethods
+  validate(:name, :presence)
 end
 
-t = Test.new
-t.number = '123'
-t.validate_all(:number) # => SUCCESS SUCCESS SUCCESS
+k = Klass.new
